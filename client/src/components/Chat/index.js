95ed306d355a99cd6d39cb3417af28326639f071
ZacWarner <zektgn@gmail.com>
2019-08-10 11:50:59 -0700

    started working on chat layout

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Input } from 'reactstrap';
+
 
 export default class Chat extends React.Component {
 
@@ -6,7 +8,17 @@ export default class Chat extends React.Component {
 
     render() {
         return (
-            
+            <Card>
+                <CardHeader>{this.props.title}</CardHeader>
+                <CardBody>
+
+                </CardBody>
+                <CardFooter>
+                    <Input></Input>
+                </CardFooter>
+
+            </Card>
+
         )
     }
 }
