57982a42ce6c616b1ed722412d3a77e4e5f7b775
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-08-10 13:56:06 -0700

    Basic Layout for Profile Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import RegForm from './components/NewUserForm';
 import Landing from './pages/Landing';
-// import Profile from './pages/Profile';
+import Profile from './pages/Profile';
 // import Login from './pages/Login';
 import Signup from './pages/Registration';
 import Dashboard from './pages/Dashboard';
@@ -15,7 +14,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Landing} />
-          {/* <Route exact path='/profile' component={Profile} /> */}
+          <Route exact path='/profile' component={Profile} />
           {/* <Route exact path='/login' component={Login} /> */}
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/dashboard/:id' component={Dashboard} />
