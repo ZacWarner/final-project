ba202db5316dd8c4efb1e0491baef89ca35ae395
Patrick Owens <manbearpat@gmail.com>
2019-08-10 12:45:20 -0700

    added defaults for valid and invalid, changed state field to a dropdown selector

--- client/src/components/NewUserForm/index.js
+++ client/src/components/NewUserForm/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Col, Row, Button, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon, Jumbotron, Container } from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, FormFeedback, Label, Input, InputGroup, InputGroupAddon, Jumbotron, Container } from 'reactstrap';
 import '../styles/newuserform.scss';
 import '../styles/variables.scss';
 
@@ -16,13 +16,15 @@ export default class RegForm extends React.Component {
                 <Col md={3}>
                   <FormGroup>
                     <Label for="First Name">First Name</Label>
-                    <Input type="text" name="First Name" placeholder="First Name" />
+                    <Input valid type="text" name="First Name" placeholder="First Name" value={this.props.firstName}/>
+                    
+                    
                   </FormGroup>
                 </Col>
                 <Col md={3}>
                   <FormGroup>
                     <Label for="Last Name">Last Name</Label>
-                    <Input type="text" name="Last Name" placeholder="Last Name" />
+                    <Input invalid type="text" name="Last Name" placeholder="Last Name" value={this.props.lastName}/>
                   </FormGroup>
                 </Col>
               </Row>
@@ -30,7 +32,8 @@ export default class RegForm extends React.Component {
                 <Col md={3}>
                   <InputGroup>
                     <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-                    <Input placeholder="username" />
+                    <Input invalid placeholder="username" value={this.props.userName}/>
+                    <FormFeedback>That name is already taken</FormFeedback>
                   </InputGroup>
                 </Col>
 
@@ -39,13 +42,13 @@ export default class RegForm extends React.Component {
                 <Col md={4}>
                   <FormGroup>
                     <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" value={this.props.email}/>
                   </FormGroup>
                 </Col>
                 <Col md={3}>
                   <FormGroup>
                     <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" value={this.props.password}/>
                   </FormGroup>
                 </Col>
               </Row>
@@ -53,7 +56,7 @@ export default class RegForm extends React.Component {
                 <Col md={4}>
                   <FormGroup>
                     <Label for="exampleAddress">Address</Label>
-                    <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St" />
+                    <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St" value={this.props.address1}/>
                   </FormGroup>
                 </Col>
               </Row>
@@ -61,7 +64,7 @@ export default class RegForm extends React.Component {
                 <Col md={4}>
                   <FormGroup>
                     <Label for="exampleAddress2">Address 2</Label>
-                    <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor" />
+                    <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor" value={this.props.address2}/>
                   </FormGroup>
                 </Col>
               </Row>
@@ -69,23 +72,29 @@ export default class RegForm extends React.Component {
                 <Col md={3}>
                   <FormGroup>
                     <Label for="exampleCity">City</Label>
-                    <Input type="text" name="city" id="exampleCity" />
+                    <Input type="text" name="city" id="exampleCity" placeholder="City" value={this.props.city}/>
                   </FormGroup>
                 </Col>
                 <Col md={1}>
                   <FormGroup>
                     <Label for="exampleState">State</Label>
-                    <Input type="text" name="state" id="exampleState" />
+                    <Input type="select" name="state" id="exampleState" value={this.props.stateProvince}>
+                    <option>Select</option>
+                    <option>AL</option>
+                    <option>AZ</option>
+                    <option>AR</option>
+                    
+                    </Input>
                   </FormGroup>
                 </Col>
                 <Col md={2}>
                   <FormGroup>
                     <Label for="exampleZip">Zip</Label>
-                    <Input type="text" name="zip" id="exampleZip" />
+                    <Input type="text" name="zip" id="exampleZip" placeholder="5-digit Zip" value={this.props.zip}/>
                   </FormGroup>
                 </Col>
               </Row>
-              <Button>Register</Button>
+              <Button onClick={this.props.user}>Register</Button>
             </Form>
           </Container>
         </Jumbotron>
