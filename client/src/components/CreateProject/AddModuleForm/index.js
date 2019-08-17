608a1b52e95cdf96bbe790d8e66dccdc664ad04e
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-08-17 02:04:01 -0700

    update

--- client/src/components/CreateProject/AddModuleForm/index.js
+++ client/src/components/CreateProject/AddModuleForm/index.js
@@ -15,7 +15,8 @@ const addModule = (props) => {
                             <FormGroup>
                                 <Label for="projName">Module Name: </Label>
                                 <Input type="text" id="modName" name="modName" value={props.data.modName} placeholder="Module Name"
-                                    onChange={props.handleInputChange} />
+                                    onChange={props.handleInputChange}
+                                    disabled={(props.data.projCreated === "no") ? true : false} />
                             </FormGroup>
                         </Col>
                         <Col>
@@ -27,6 +28,7 @@ const addModule = (props) => {
                                     id="modDueDate"
                                     value={props.data.modDueDate}
                                     onChange={props.handleInputChange}
+                                    disabled={(props.data.projCreated === "no") ? true : false}
                                 />
                             </FormGroup>
                         </Col>
@@ -35,28 +37,27 @@ const addModule = (props) => {
                         <Col>
                             <FormGroup>
                                 <Label for="projDesc">Module Description:</Label>
-                                <Input type="textarea" name="modDesc" id="modDesc" value={props.data.modDesc} onChange={props.handleInputChange} />
+                                <Input type="textarea" name="modDesc" id="modDesc" value={props.data.modDesc}
+                                    onChange={props.handleInputChange}
+                                    disabled={(props.data.projCreated === "no") ? true : false} />
                             </FormGroup>
-                            <Button color="primary" onClick={props.handleAddModule}>Add Module</Button>
+                            <Button color="primary" onClick={props.handleAddModule}
+                                disabled={(props.data.projCreated === "no") ? true : false}>Add Module</Button>
                         </Col>
                         <Col>
                             <FormGroup>
                                 <Label for="assignDev">Assign To:</Label>
                                 <Input type="select" name="modDev" id="assignDev"
-                                    value={props.data.modDev} onChange={props.handleInputChange}>
+                                    value={props.data.modDev} onChange={props.handleInputChange}
+                                    disabled={(props.data.projCreated === "no") ? true : false}>
                                     <option>-- select developer --</option>
-                                    {/* To be loaded from profile database */}
-                                    <option>Meena</option>
-                                    <option>Zac</option>
-                                    <option>Patrick</option>
-                                    <option>Adam</option>
-                                    <option>Lucious</option>
+                                    <option>check</option>
                                 </Input>
                             </FormGroup>
                             <FormGroup check>
                                 <Label check>
                                     <Input type="checkbox" defaultChecked={props.data.level1}
-                                        onChange={props.handleCheckBox} />{' '}
+                                        onChange={props.handleCheckBox} disabled={(props.data.projCreated === "no") ? true : false} />{' '}
                                     Level 1 Module
                                     </Label>
                             </FormGroup>
@@ -65,11 +66,7 @@ const addModule = (props) => {
                                     disabled={props.data.level1 === true ? true : false}
                                     value={props.data.modParent} onChange={props.handleInputChange}>
                                     <option>-- select parent --</option>
-                                    {props.data.modules.map((e, key) => {
-                                        if (e.level1module) {
-                                            return <option key={key} value={e.mod_name}>{e.mod_name}</option>;
-                                        }
-                                    })}
+                                    <option>check</option>
                                 </Input>
                             </FormGroup>
                         </Col>
