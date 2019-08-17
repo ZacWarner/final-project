0de3048be79478df02dabc176cc56863c05a8d3c
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-08-16 17:32:24 -0700

    update

--- client/src/components/CreateProject/ModuleCard/index.js
+++ client/src/components/CreateProject/ModuleCard/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {
     Card, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, Button, Row, Col
 } from 'reactstrap';
+import Moment from 'moment';
 
 const addModule = (props) => {
     return (
@@ -12,10 +13,20 @@ const addModule = (props) => {
             {console.log(props.data)}
             <CardBody>
                 <CardTitle><h5>Module Name: {props.data.mod_name}</h5><hr /></CardTitle>
-                {/* <CardSubtitle><h6>Project Owner: {props.data.developer}</h6></CardSubtitle> */}
-                <p>Description: {props.data.mod_description}</p>
-                <p>Due Date: {props.data.mod_due}</p>
-                <p>Assigned to: {props.data.developer}</p>
+                <Row>
+                    <Col>
+                        <p>Assigned to: {props.data.developer}</p>
+                    </Col>
+                    <Col>
+                        <p>Due Date: {Moment(props.data.mod_due).format('YYYY-MM-DD')}</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Description: {props.data.mod_description}</p>
+                    </Col>
+                </Row>
+
                 <Button color="primary">Delete</Button>
             </CardBody>
         </Card>
