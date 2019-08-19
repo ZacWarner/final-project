0e61e12d773129144c952f2a7bd59375ccea7093
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-08-18 23:43:25 -0700

    update

--- client/src/components/Dashboard/Projsummary/index.js
+++ client/src/components/Dashboard/Projsummary/index.js
@@ -1,26 +1,75 @@
 import React, { Component } from "react";
 import {
-    Card, CardText, CardBody,
+    Card, CardBody,
     CardTitle, CardSubtitle
 } from 'reactstrap';
+import Moment from 'moment';
+import API from '../../../utils/API';
 
 class Projsummary extends Component {
 
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            proj_name: "",
+            proj_owner: "",
+            start_date: "",
+            due_date: "",
+            overallPercentage: ""
+        }
+    }
 
+    componentDidMount() {
+        let overallPercentage = 0;
+        API.getProject(this.props.projId)
+            .then(res => {
+                let moduleCount = 0, moduleComplete = 0;
+                if (res.data.modules) {
+                    res.data.modules.forEach(i => {
+                        if (i.level1module) {
+                            moduleCount++;
+                            let subcount = 0, subcomplete = 0;
+                            res.data.modules.forEach(j => {
+                                if (i.mod_name === j.parent) {
+                                    subcount++;
+                                    if (j.complete) {
+                                        subcomplete++;
+                                    }
+                                }
+                            });
+                            console.log("subcount: " + subcount + " subcomplete: " + subcomplete);
+                            if (subcount === 0) {
+                                if (i.complete) {
+                                    moduleComplete++;
+                                }
+                            } else if (subcount === subcomplete) {
+                                moduleComplete++;
+                            }
+
+                        }
+                    });
+                    overallPercentage = ((moduleComplete / moduleCount) * 100).toFixed(1);
+                }
+                this.setState({
+                    proj_name: res.data.proj_name,
+                    proj_owner: res.data.proj_owner,
+                    start_date: res.data.start_date,
+                    due_date: res.data.due_date,
+                    overallPercentage: overallPercentage
+                });
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
             <Card className="m-1 card-props">
                 <CardBody>
-                    <CardTitle><h5>Project Name</h5><hr /></CardTitle>
-                    <CardSubtitle><h6>Project Owner</h6></CardSubtitle>
-                    <CardText>
-                        <p>Start Date: ***</p>
-                        <p>End Date: ***</p>
-                        <p>Overall Status: ** %</p>
-                    </CardText>
+                    <CardTitle><h4>{this.state.proj_name}</h4><hr /></CardTitle>
+                    <CardSubtitle><h6><i>{this.state.proj_owner}</i></h6></CardSubtitle>
+                    <p><b>Start Date: </b>{Moment(this.state.start_date).format('YYYY-MM-DD')}</p>
+                    <p><b>End Date: </b>{Moment(this.state.due_date).format('YYYY-MM-DD')}</p>
+                    <p><b>Progress: </b>{this.state.overallPercentage}%</p>
                 </CardBody>
             </Card>
         );
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,51 +1,13 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import Navbar from '../../components/Navbar';
 import Pie from '../../components/Dashboard/Pie';
 import Tree from '../../components/Dashboard/Tree';
 import Projphase from '../../components/Dashboard/Stepprogress';
 import Projsummary from '../../components/Dashboard/Projsummary';
 import Moduleprogress from '../../components/Dashboard/Moduleprogress';
-import API from '../../utils/API';
 
 class Dashboard extends Component {
-
-    state = {
-        pieData: {},
-        treeData: {},
-        projSum: {},
-        overallPercentage: 0
-    };
-
-    componentDidMount = () => {
-        //Grab Data from db
-        console.log("Proj id: " + this.props.match.params.id);
-        API.getProject(this.props.match.params.id)
-            .then(res => {
-                console.log(res);
-                this.overallstatus(res);
-                this.setState({
-                    projSum: this.dataSummary(res)
-                });
-                console.log(this.state.projSum);
-
-            })
-            .catch(err => console.log(err));
-    }
-
-    overallstatus(res) {
-
-    }
-
-    dataSummary(res) {
-        let summary = {};
-        summary.projName = res.data.proj_name;
-        summary.projOwner = res.data.proj_owner;
-        summary.start_date = res.data.start_date;
-        summary.due_date = res.data.due_date;
-
-        return summary;
-    }
 
     render() {
         return (
@@ -54,16 +16,14 @@ class Dashboard extends Component {
                 <Container>
                     <Row className="mt-3">
                         <Col md="3">
-                            <Projsummary
-
-                            />
+                            <Projsummary projId={this.props.match.params.id} />
                         </Col>
                         <Col className="text-center">
-                            <Projphase />
+                            <Projphase projId={this.props.match.params.id} />
                         </Col>
                         <Col md="3" className="text-center">
                             <Pie
-                                data={this.state.pieData}
+                                projId={this.props.match.params.id}
                                 width={140}
                                 height={140}
                                 innerRadius={35}
@@ -73,10 +33,10 @@ class Dashboard extends Component {
                     </Row>
                     <Row>
                         <Col md="6" className="text-center">
-                            <Tree data={this.state.treeData} />
+                            <Tree projId={this.props.match.params.id} />
                         </Col>
                         <Col className="text-center">
-                            <Moduleprogress />
+                            <Moduleprogress projId={this.props.match.params.id} />
                         </Col>
                     </Row>
                 </Container>
