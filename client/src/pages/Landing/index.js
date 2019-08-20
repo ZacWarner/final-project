a016a049d671fff4d04deca5fa7da7becf4b0a50
Patrick Owens <manbearpat@gmail.com>
2019-08-19 18:43:06 -0700

    began working on modal mechanics

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -6,6 +6,8 @@ import {
     Container,
 
 } from 'reactstrap';
+import '../../components/styles/Landing/jumbotron.scss';
+import '../../components/styles/variables.scss';
 
 
 class Landing extends Component {
@@ -13,11 +15,11 @@ class Landing extends Component {
         return (
             <>
                 <Navbar />
-                <Jumbotron>
+                <Jumbotron className="landing-jumbo">
                     <Container className="text-center">
                         <h1 className="display-3">Regulate</h1>
-                        <a href="/signup" className="btn btn-primary m-3">Sign Up</a>
-                        <a href="/login" className="btn btn-primary m-3">Login</a>
+                        <a href="/signup" className="btn btn-outline-light m-3">Sign Up</a>
+                        <a href="/login" className="btn btn-outline-light m-3">Login</a>
                     </Container>
                 </Jumbotron>
             </>
