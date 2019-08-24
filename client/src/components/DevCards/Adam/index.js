901a6cc102a1374153251ab470eb92041f04d101
unknown <adam_tran_916@hotmail.com>
2019-08-23 19:24:56 -0700

    about us updated

--- client/src/components/DevCards/Adam/index.js
+++ client/src/components/DevCards/Adam/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../styles/AboutUs/devcard.scss'
 import '../../styles/variables.scss';
-
+import { FaLinkedin } from 'react-icons/fa';
 
 
 function Adam() {
@@ -12,16 +12,16 @@ function Adam() {
                 <img src="https://media.licdn.com/dms/image/C4D03AQH1IorHq4vpvg/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=dHR0VBpKngxt5ffNW32ot5gqMkCKhCSkZD2NVUtZEMs" className="card-img-top"></img>
                 <div className="card-body">
                     <h3>Adam Tran</h3>
-                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p className="card-text">Recent graduate of the UC Davis, Full Stack Web Development program. In pursuit of attaining a front end developer career. In-depth knowledge of HTML5, CSS3, and JavaScript with the prime focus of developing unique and stunning websites that are desktop and mobile user-friendly.</p>
                     <h4>Skillset:</h4>
                     <ul>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
-                        <li>Skill</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>ReactJs</li>
                     </ul>
-                    <a href="https://www.linkedin.com/in/adam-tran-a03884141/">LinkedIn Profile</a>
+                    <a href="https://www.linkedin.com/in/adam-tran-a03884141/"><FaLinkedin className="aboutIcon" /></a>
 
                 </div>
             </div>
